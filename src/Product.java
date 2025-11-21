18e9393ef62932f46268aaaf227cf0e58fef1e98
Dang Duy Anh <duyanhhdang194@gmail.com>
2025-11-21 14:35:48 +0700

    lớp product

---
+++ src/Product.java
@@ -0,0 +1,21 @@
+import java.io.Serializable;
+
+public abstract class Product implements Serializable {
+    protected String ID;
+    protected String name;
+    protected String category;
+    protected double baseCost;
+    protected double sellPrice;
+    protected int quantity;
+
+    public Product(String ID, String name, String category, double baseCost, double sellPrice, int quantity){
+        this.ID = ID;
+        this.name = name;
+        this.category = category;
+        this.baseCost = baseCost;
+        this.sellPrice = sellPrice;
+        this.quantity = quantity;
+    }
+
+    public abstract String getDetail();
+}
