7017429b7cd0502b465137ad0b19e0535e54e587
Dang Duy Anh <duyanhhdang194@gmail.com>
2025-11-21 15:30:31 +0700

    product

--- src/Product.java
+++ src/Product.java
@@ -1,17 +1,14 @@
-import java.io.Serializable;
-
-public abstract class Product implements Serializable {
-    protected String ID;
+public abstract class Product{
+    protected String id;
     protected String name;
     protected String category;
     protected double baseCost;
     protected double sellPrice;
     protected int quantity;
 
-    public Product(String ID, String name, String category, double baseCost, double sellPrice, int quantity){
-        this.ID = ID;
+    public Product(String id, String name, double baseCost, double sellPrice, int quantity){
+        this.id = id;
         this.name = name;
-        this.category = category;
         this.baseCost = baseCost;
         this.sellPrice = sellPrice;
         this.quantity = quantity;
@@ -19,4 +16,51 @@ public abstract class Product implements Serializable {
 
     public abstract String getDetail();
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
+    public double getBaseCost() {
+        return baseCost;
+    }
+
+    public void setBaseCost(double baseCost) {
+        this.baseCost = baseCost;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
